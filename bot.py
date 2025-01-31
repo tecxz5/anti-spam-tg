f1b7171036c743db469fb2332a9e23f2d44f91fa
tecxz5 <zamulatimofei@yandex.ru>
2025-01-31 21:42:30 +0300

    максимальное упрощение антиспама

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
 import telebot
 from config import TOKEN
-from db import create_tables, update_user, get_punishments, increment_punishments, add_message, get_recent_messages
+from db import create_tables, add_message, get_recent_messages
 import time
 
 bot = telebot.TeleBot(TOKEN)
 
-# Функция для мутирования пользователя
 def mute_user(chat_id, user_id, duration):
     bot.restrict_chat_member(chat_id, user_id, until_date=int(time.time()) + duration, can_send_messages=False)
 
-# Обработчик текстовых сообщений
 @bot.message_handler(content_types=['text', 'sticker', 'animation'])
 def handle_message(message):
     user_id = message.from_user.id
@@ -17,24 +15,13 @@ def handle_message(message):
     content = message.text if message.content_type == 'text' else message.content_type
     
     create_tables(chat_id)
-    update_user(chat_id, user_id)
     add_message(chat_id, user_id, content)
     
     recent_messages = get_recent_messages(chat_id, user_id)
     
     if len(recent_messages) > 5 and all(msg[0] == content for msg in recent_messages):
-        increment_punishments(chat_id, user_id)
-        punishments = get_punishments(chat_id, user_id)
-        mute_durations = [60, 300, 600]
-        
-        if punishments > 3:
-            duration = mute_durations[2]
-        elif punishments == 3:
-            duration = mute_durations[1]
-        else:
-            duration = mute_durations[0]
-        
-        mute_user(chat_id, user_id, duration)
-        bot.send_message(chat_id, f"Пользователь {message.from_user.first_name} замучен на {duration // 60} минут(ы) за спам.")
+        mute_user(chat_id, user_id, 60)
+        user_link = f'<a href="tg://user?id={user_id}">{message.from_user.first_name}</a>'
+        bot.send_message(chat_id, f"Пользователь {user_link} замучен на минуту за спам.", parse_mode='HTML')
 
 bot.infinity_polling()
